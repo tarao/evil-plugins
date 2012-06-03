9d13859ecb9a3f668dc1d208d48835adc11aece2
tarao <tarao.gnn@gmail.com>
2012-06-03 12:59:58 +0000

    Use evil-find-char.

--- evil-textobj-between.el
+++ evil-textobj-between.el
@@ -14,20 +14,31 @@
   :type 'string
   :group 'evil-textobj-between)
 
-(defun evil-between-range (count &optional inclusive)
+(defun evil-between-range (count beg end type &optional inclusive)
   (condition-case ()
-      (let* ((ch (read-char))
-             (regexp (format "[%s]" (string ch))))
-        (evil-regexp-range count regexp regexp (not inclusive)))))
+      (let ((ch (read-char)) beg-inc end-inc)
+        (save-excursion
+          (goto-char end)
+          (backward-char)
+          (evil-find-char count ch)
+          (setq end-inc (1+ (point)))
+          (goto-char beg)
+          (evil-find-char-backward count ch)
+          (setq beg-inc (point)))
+        (if inclusive
+            (evil-range beg-inc end-inc)
+          (evil-range
+           (if (= (1+ beg-inc) beg) beg-inc (1+ beg-inc))
+           (if (= (1- end-inc) end) end-inc (1- end-inc)))))))
 
-(evil-define-text-object evil-a-between (count)
+(evil-define-text-object evil-a-between (count &optional beg end type)
   "Select range between a character by which the command is followed."
   :extend-selection t
-  (evil-between-range count t))
-(evil-define-text-object evil-inner-between (count)
+  (evil-between-range count beg end type t))
+(evil-define-text-object evil-inner-between (count &optional beg end type)
   "Select inner range between a character by which the command is followed."
   :extend-selection nil
-  (evil-between-range count))
+  (evil-between-range count beg end type))
 
 (define-key evil-outer-text-objects-map evil-textobj-between-a-key
   'evil-a-between)
