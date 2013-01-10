0a2c641c84df58bc2ad46815b83f422a3f423977
tarao <tarao.gnn@gmail.com>
2013-01-11 08:53:06 +0900

    Add documentation for evil-mode-line.el.

--- evil-mode-line.el
+++ evil-mode-line.el
@@ -12,7 +12,9 @@
     (operator . "DarkSeaGreen4")
     (visual   . "SteelBlue4")
     (emacs    . "#8c5353"))
-  "Mode line color corresponds to Evil state.")
+  "Mode line color corresponds to Evil state."
+  :type '(alist :key-type symbol :value-type string)
+  :group 'evil-mode-line)
 (defcustom evil-normal-state-msg ""
   "Mode line message for Evil normal state."
   :type 'string
