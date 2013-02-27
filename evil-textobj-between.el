70bcd783a61a5b7459cf23a001faabee10d8542b
tarao <tarao.gnn@gmail.com>
2013-02-27 11:34:18 +0900

    Make `evil-between-range' work without current selection.

--- evil-textobj-between.el
+++ evil-textobj-between.el
@@ -41,21 +41,22 @@
   :group 'evil-textobj-between)
 
 (defun evil-between-range (count beg end type &optional inclusive)
-  (condition-case ()
-      (let ((ch (read-char)) beg-inc end-inc)
-        (save-excursion
-          (goto-char end)
-          (backward-char)
-          (evil-find-char count ch)
-          (setq end-inc (1+ (point)))
-          (goto-char beg)
-          (evil-find-char-backward count ch)
-          (setq beg-inc (point)))
-        (if inclusive
-            (evil-range beg-inc end-inc)
-          (evil-range
-           (if (= (1+ beg-inc) beg) beg-inc (1+ beg-inc))
-           (if (= (1- end-inc) end) end-inc (1- end-inc)))))))
+  (ignore-errors
+    (let ((ch (evil-read-key)) beg-inc end-inc)
+      (save-excursion
+        (when end (goto-char end))
+        (backward-char)
+        (evil-find-char count ch)
+        (setq end-inc (1+ (point))))
+      (save-excursion
+        (when beg (goto-char beg))
+        (evil-find-char-backward count ch)
+        (setq beg-inc (point)))
+      (if inclusive
+          (evil-range beg-inc end-inc)
+        (evil-range
+         (if (and beg (= (1+ beg-inc) beg)) beg-inc (1+ beg-inc))
+         (if (and end (= (1- end-inc) end)) end-inc (1- end-inc)))))))
 
 (evil-define-text-object evil-a-between (count &optional beg end type)
   "Select range between a character by which the command is followed."
